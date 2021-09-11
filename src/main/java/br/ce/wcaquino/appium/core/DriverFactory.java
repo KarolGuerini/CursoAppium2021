22fe8832006fa68db8fb2b127749bafd52c526ce
KarolGuerini <karoltech20@gmail.com>
2021-09-11 18:19:33 -0300

    DriverFactory, Before/After e DSL

---
+++ src/main/java/br/ce/wcaquino/appium/core/DriverFactory.java
@@ -0,0 +1,43 @@
+package br.ce.wcaquino.appium.core;
+
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
+public class DriverFactory {
+    public static AndroidDriver<MobileElement> driver;
+
+    public static AndroidDriver<MobileElement> getDriver()  {
+        if (driver == null) {
+            creatDriver();
+        }
+        return driver;
+    }
+
+    private static void creatDriver() {
+
+        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+        desiredCapabilities.setCapability("app", "/home/karol/IdeaProjects/cursoAppiumWA/src/main/resources/CTAppium_1_2.apk");
+        desiredCapabilities.setCapability("platformName", "Android");
+        desiredCapabilities.setCapability("platformVersion", "7.1.1");
+        desiredCapabilities.setCapability("deviceName", "0032320614");
+
+        try {
+            driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), desiredCapabilities);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
+
+    public static void killDriver(){
+        if(driver != null){
+            driver.quit();
+            driver = null;
+        }
+    }
+}
---
+++ src/main/java/br/ce/wcaquino/appium/core/Generics.java
@@ -0,0 +1,16 @@
+package br.ce.wcaquino.appium.core;
+
+import org.openqa.selenium.By;
+
+import static br.ce.wcaquino.appium.core.DriverFactory.getDriver;
+
+public class Generics {
+
+    public void escrever(By by, String texto){
+        getDriver().findElement(by).sendKeys(texto);
+    }
+
+    public String obterTexto(By by){
+        return getDriver().findElement(by).getText();
+    }
+}
